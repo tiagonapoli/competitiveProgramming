88a9b502f5df1bbb7ec6f6b1e626efcdeadaa7b0
Tiago Martins Nápoli <napoli.tiago96@gmail.com>
2020-10-18 13:43:42 -0300

    nit on template
    Signed-off-by: Tiago Martins Nápoli <napoli.tiago96@gmail.com>

--- template.cpp
+++ template.cpp
@@ -21,10 +21,8 @@ const ll MOD = 1000000007;
 const double PI = acos(-1.0);
 const double eps = 1e-9;
 using namespace std;
-int nxt() { int x; scanf("%d", &x); return x; }
+inline int nxt() { int x; scanf("%d", &x); return x; }
 #define N 100100
-
-
 
 
 int main () {
